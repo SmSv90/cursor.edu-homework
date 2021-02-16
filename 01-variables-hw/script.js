99b2781c1ce06472dd4770792c982f26c33d553b
SmSv90 <smr.stv@gmail.com>
2021-02-16 18:10:36 +0200

    finish tasks

--- 01-variables-hw/script.js
+++ 01-variables-hw/script.js
@@ -26,30 +26,40 @@ const costProductOne = 15.678,
       costProductTwo = 123.965, 
       costProductThree = 90.2345;
 
-const maxPrice = Math.max(costProductOne, costProductTwo, costProductThree);
-const minPrice = Math.min(costProductOne, costProductTwo, costProductThree);
+const maxPrice = Math.max(costProductOne, costProductTwo, costProductThree).toFixed(2);
+const minPrice = Math.min(costProductOne, costProductTwo, costProductThree).toFixed(2);
 
-const fullPrise = costProductOne + costProductTwo + costProductThree;
+const fullPrice = (costProductOne + costProductTwo + costProductThree).toFixed(2);
 
 const roundToMinPrice = Math.floor(costProductOne) + Math.floor(costProductTwo) + Math.floor(costProductThree);
-const roundTo100Price = Math.round(fullPrise/100)*100;
+const roundTo100Price = Math.round(fullPrice/100)*100;
 
-let priseIsEven = false;
-if (Math.floor(fullPrise) % 2 == 0) {
-  priseIsEven = true;
+let priceIsEven = false;
+
+if (Math.floor(fullPrice) % 2 == 0) {
+  priceIsEven = true;
 };
 
-const balanceOfMoney = 500 - fullPrise;
+const balanceOfMoney = 500 - fullPrice;
 
-const avarageCostProductOne = costProductOne.toFixed(2);
-const avarageCostProductTwo = costProductTwo.toFixed(2);
-const avarageCostProductThree = costProductThree.toFixed(2);
+const avarageCostPrice= ((costProductOne + costProductTwo + costProductThree) / 3).toFixed(2);
 
-console.log(avarageCostProductOne);
+
+const discount = (Math.random() * fullPrice).toFixed(2);
+const priceWithDiscount = (fullPrice - discount).toFixed(2);
+const proffit = (fullPrice / 2 - (fullPrice - priceWithDiscount)).toFixed(2);
 
 const answer = 
 `Максимальна ціна: ${maxPrice},
-Мінімальна ціна: ${minPrice}`;
+Мінімальна ціна: ${minPrice},
+Вартість всіх товарів: ${fullPrice},
+Ціла частина вартості всіх товарів: ${roundToMinPrice},
+Сума товарів округлена до сотень: ${roundTo100Price},
+Чи є сума всіх товарів (округлена в меншу сторону) парним числом?: ${priceIsEven},
+Сума решти, при оплаті всіх товарів (без округлення), якщо клієнт платить 500 грн: ${balanceOfMoney},
+Середнє значення цін товарыв: ${avarageCostPrice},
+Знижка: ${discount},
+Cума до оплати зі знижкою: ${priceWithDiscount},
+Чистий прибуток, якщо клієнт заплатив зі знижкою та собівартість товарів рівно в два рази нижче їх ціни: ${proffit}`;
 
 console.log(answer);
-// alert(answer);
