9b33b9adfb96f335d943519aa58571caaa871b31
SmSv90 <smr.stv@gmail.com>
2021-02-20 23:16:59 +0200

    finish hw #2

---
+++ 02-loops-hw/script.js
@@ -0,0 +1,27 @@
+let numberN, numberM, isEvenSkip;
+let sum = 0;
+
+do {
+  numberN = prompt("Введіть ціле число N");
+  numberM = prompt("Введіть ціле число M");
+  isEvenSkip = confirm("Бажаєте пропускати парні числа?");
+  if (numberN !== null && numberM !== null) {
+    numberN = +numberN;
+    numberM = +numberM;
+  };
+  if (!Number.isInteger(numberN) || !Number.isInteger(numberM)) {
+    alert("Ви вказали не вірні значення чисел");
+  };
+} while (!Number.isInteger(numberN) || !Number.isInteger(numberM));
+
+for (let i = numberN; i <= numberM; i++) {
+  if (isEvenSkip) {
+    if (i % 2 !== 0) {
+      sum += i;
+    }
+  } else {
+    sum += i;
+  }
+};
+
+alert(`Сума чисел становить: ${sum}`);
