890a5f53d82aaea2015ccae4c22cbc13b3afae50
SmSv90 <smr.stv@gmail.com>
2021-03-09 12:47:06 +0200

    some fix

---
+++ 06-objects-hw/script.js
@@ -0,0 +1,44 @@
+const students = [{
+  name: "Tanya",
+  course: 3,
+  subjects: {
+    math: [4, 4, 3, 4],
+    algorithms: [3, 3, 3, 4, 4, 4],
+    data_science: [5, 5, 3, 4]
+  }
+}, {
+  name: "Victor",
+  course: 4,
+  subjects: {
+    physics: [5, 5, 5, 3],
+    economics: [2, 3, 3, 3, 3, 5],
+    geometry: [5, 5, 2, 3, 5]
+  }
+}, {
+  name: "Anton",
+  course: 2,
+  subjects: {
+    statistics: [4, 5, 5, 5, 5, 3, 4, 3, 4, 5],
+    english: [5, 3],
+    cosmology: [5, 5, 5, 5]
+  }
+}];
+
+/*
+Створіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"] - яка повертає список предметів для конкретного студента. Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл
+*/
+
+function getSubjects(students) {
+  let i = prompt('Choose number of students from 1 to 3');
+
+  let arraySubjects = Object.keys(students[i-1].subjects).map( subject => {
+    return (subject.charAt(0).toUpperCase() + subject.slice(1)).replace('_', ' ');
+  });
+
+  
+  
+  return arraySubjects.join(', ');
+
+}
+
+console.log(getSubjects(students));
