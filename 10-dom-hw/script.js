553b6e1256a5e97f68a35746d626acdd2cc34b27
SmSv90 <smr.stv@gmail.com>
2021-03-16 22:23:51 +0200

    HW #10

---
+++ 10-dom-hw/script.js
@@ -0,0 +1,43 @@
+const blockCSSList = 'height: 50px; width: 50px',
+  wrapperCSSList = 'display: flex; flex-wrap: wrap; width: 250px; height: 250px; border: 1px solid black; margin-top: 10px;',
+  wrapperBlockLimit = 25;
+
+function getRandomColor() {
+  const red = Math.trunc(Math.random() * 255);
+  const green = Math.trunc(Math.random() * 255);
+  const blue = Math.trunc(Math.random() * 255);
+
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function generateBlocks() {
+  const wrapper = document.createElement('div');
+  wrapper.style.cssText = wrapperCSSList;
+  wrapper.setAttribute('id', 'wrapper');
+  document.body.append(wrapper);
+
+  for (let i = 0; i < wrapperBlockLimit; i++) {
+    const block = document.createElement('div');
+    block.style.cssText = blockCSSList;
+    block.style.backgroundColor = getRandomColor();
+    wrapper.append(block);
+  }
+
+  document.querySelector('#generateBlocks').disabled = true;
+}
+
+function generateBlocksInterval() {
+  document.querySelector('#generateBlocksInterval').disabled = true;
+  setInterval(() => {
+    if (!document.querySelector('#wrapper')) {
+      generateBlocks();
+    }
+    document.querySelector('#wrapper').remove();
+    generateBlocks();
+  }, 1000);
+}
+
+function resetPage() {
+  document.location.reload();
+}
+
