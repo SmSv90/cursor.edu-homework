f2ecfc7293906f749c13f550b3750ed82d0ad1b8
SmSv90 <smr.stv@gmail.com>
2021-03-22 22:23:43 +0200

    finish HW #11

---
+++ 11-events-hw/script.js
@@ -0,0 +1,33 @@
+// play sound when pressed key on keyboard
+window.addEventListener('keydown', function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.code}"]`);
+  const key = document.querySelector(`button[data-key="${e.code}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0.2;
+  audio.play();
+
+  key.classList.add('playing');
+  setTimeout(() => {
+    key.classList.remove('playing');
+  }, 1500);
+});
+
+// additional interaction. play sound when click key by mouse
+window.addEventListener('click', function (e) {
+  const key = e.target.closest('button');
+
+  if (!key) return;
+
+  const dataKey = key.getAttribute('data-key');
+  const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0.2;
+  audio.play();
+
+  key.classList.add('playing');
+  setTimeout(() => {
+    key.classList.remove('playing');
+  }, 1500);
+})
