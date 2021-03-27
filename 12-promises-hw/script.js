9e4cca9a04b08c822e6b8f6faa1fc59a0b9e8534
SmSv90 <smr.stv@gmail.com>
2021-03-27 21:45:45 +0200

    HW #12

---
+++ 12-promises-hw/script.js
@@ -0,0 +1,22 @@
+async function getRandomChinese(length) {
+  let result = '';
+  let index = 0;
+
+  while (index < length) {
+    let promise = new Promise(resolve => {
+      const sign = Date.now().toString().substr(-5);
+      result += String.fromCharCode(sign);
+
+      setTimeout(() => {
+        resolve(result);
+      }, 50)
+    });
+
+    await promise;
+    index++;
+  }
+
+  console.log(result);
+}
+
+getRandomChinese(4);
