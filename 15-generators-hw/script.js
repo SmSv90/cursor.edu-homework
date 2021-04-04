d5be3b914f18b50542c20110d9fb1fb367fbead0
SmSv90 <smr.stv@gmail.com>
2021-04-04 22:43:14 +0300

    HW #15 finished

---
+++ 15-generators-hw/script.js
@@ -0,0 +1,49 @@
+// Function 1
+function* createIdGenerator() {
+  let i = 1;
+
+  while (true) {
+    yield i++;
+  }
+}
+
+// Function 2
+function* newFontGenerator(fontSize) {
+  let size = fontSize;
+  createText(size);
+
+  while (true) {
+    let state = yield;
+
+    if (state == "up") {
+      size += 2;
+    } else if (state == "down") {
+      size -= 2;
+    }
+    createText(size);
+  }
+}
+
+function createText(size) {
+  const textWrapper = document.createElement('div');
+  textWrapper.style.fontSize = `${size}px`;
+  textWrapper.innerHTML = `this text is ${size}px size`;
+  document.body.append(textWrapper);
+}
+
+const idGenerator = createIdGenerator();
+document.writeln(`<b>Function 1:</b><br>`)
+document.writeln(`1st request of generator: ${idGenerator.next().value}<br>`);
+document.writeln(`2nd request of generator: ${idGenerator.next().value}<br>`);
+document.writeln(`3rd request of generator: ${idGenerator.next().value}<br>`);
+
+document.writeln(`<b>Function 2:</b><br>`);
+const fontGenerator = newFontGenerator(14); // 14 – стартове значення
+fontGenerator.next("up").value;
+fontGenerator.next("up").value;
+fontGenerator.next("up").value;
+fontGenerator.next().value;
+fontGenerator.next("down").value;
+fontGenerator.next("down").value;
+fontGenerator.next("down").value;
+fontGenerator.next().value;
