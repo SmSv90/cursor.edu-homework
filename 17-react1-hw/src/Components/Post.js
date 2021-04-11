c4384e22f0ce4d10b5d166a768cd66cba42fe849
SmSv90 <smr.stv@gmail.com>
2021-04-11 18:50:58 +0300

    fix logic blocks dependency

--- 17-react1-hw/src/Components/Post.js
+++ 17-react1-hw/src/Components/Post.js
@@ -6,18 +6,18 @@ const Post = (props) => {
     <div className="wrapper">
       <div className="author">
         <div className="author-avatar"><img src={props.author.photo} alt="avatar" /></div>
-        <div className="author-content">
-          <div className="author-info">
-            <div className="author-name">{props.author.name}</div>
-            <div className="author-icon"><img src={`${process.env.PUBLIC_URL}/assets/approval-symbol-in-badge.png`} alt="check" /></div>
-            <div className="author-nickname">{props.author.nickname}</div>
-            <div className="date">{props.date}</div>
-            <div className="info-option"><img src={`${process.env.PUBLIC_URL}/assets/down-chevron.png`} alt="arrow-down" /></div>
-          </div>
-          <div className="content-main">{props.content}</div>
+        <div className="author-info">
+          <div className="author-name">{props.author.name}</div>
+          <div className="author-icon"><img src={`${process.env.PUBLIC_URL}/assets/approval-symbol-in-badge.png`} alt="check" /></div>
+          <div className="author-nickname">{props.author.nickname}</div>
+          <div className="date">{props.date}</div>
+          <div className="info-option"><img src={`${process.env.PUBLIC_URL}/assets/down-chevron.png`} alt="arrow-down" /></div>
         </div>
       </div>
-      <div className="image"><img src={props.image} alt="image" /></div>
+      <div className="content">
+        <div className="content-text">{props.content}</div>
+        <div className="content-image"><img src={props.image} alt="image" /></div>
+      </div>
       <div className="social">
         <div className="social-comment"><img src={`${process.env.PUBLIC_URL}/assets/speech-bubble.png`} alt="comment" /> {props.comment}</div>
         <div className="social-retweet"><img src={`${process.env.PUBLIC_URL}/assets/retweet.png`} alt="retweet" /> {props.retweet}</div>
